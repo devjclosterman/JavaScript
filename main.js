408de39c007c81a2d64d715972e67320f483669f
Jason Closterman <devjclosterman@gmail.com>
2023-05-26 16:17:32 -0700

    level up with basics

--- main.js
+++ main.js
@@ -3,3 +3,26 @@ import { square, cube } from './math.js'
 console.log('Square of 5:', square(5));
 console.log('Cube of 5:', cube(5));
 
+class Animal {
+    constructor(name) {
+        this.name;
+    }
+    speak() {
+        console.log(this.name + ' makes a sound.');
+    }
+}
+
+class Dog extends Animal {
+      contructor(name) {
+       super(name);
+ }
+
+ speak() {
+    console.log(this.name + ' barks.');
+ }
+}
+
+const dog = new Dog('buddy');
+dog.speak();
+
+
